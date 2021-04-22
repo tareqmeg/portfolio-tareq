586a4deae3d7e5c7849ac8309b99b18264080726
tareqmeg <tareq.megdadi@gmail.com>
2021-04-22 22:45:29 +0300

    lab 5

---
+++ server.js
@@ -0,0 +1,27 @@
+'use strict';
+
+require('dotenv').config();
+const express = require('express');
+const server = express();
+
+const PORT = process.env.PORT || 3000;
+
+// server.use(express.static('./public'));
+server.use(express.static('./public'));
+
+server.get('/hello', (request, response) => {
+  response.status(200).send('Hello');
+});
+
+server.get('/data', (request, response) => {
+  let airplanes = {
+    departure: Date.now(),
+    canFly: true,
+    pilot: 'Well Trained',
+  };
+  response.status(200).json(airplanes);
+});
+
+server.use('*', (request, response) => response.send('Sorry, that route does not exist.'));
+
+server.listen(PORT,() => console.log(`Listening on port ${PORT}`));
